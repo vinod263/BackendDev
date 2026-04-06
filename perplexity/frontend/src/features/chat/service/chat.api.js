f69356b0c046a07aa4377d6bbf0747d8e617785e
Vinod Gayari <gayarivinod263@gmail.com>
2026-04-06 21:22:38 +0530

    final bug fix in axios

--- perplexity/frontend/src/features/chat/service/chat.api.js
+++ perplexity/frontend/src/features/chat/service/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000 || https://backenddev-0f71.onrender.com/",
+  baseURL: "http://localhost:3000 ",
   withCredentials: true,
 });
 
