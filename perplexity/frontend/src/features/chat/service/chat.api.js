3b96df132e487e2f821a55b2ef1311635ec1db6e
Vinod Gayari <gayarivinod263@gmail.com>
2026-04-06 22:04:13 +0530

    url env

--- perplexity/frontend/src/features/chat/service/chat.api.js
+++ perplexity/frontend/src/features/chat/service/chat.api.js
@@ -1,9 +1,13 @@
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000 ",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+
 
 export const sendMessage = async ({ message, chatId }) => {
     const response = await api.post("/api/chats/message", { message, chat: chatId })
