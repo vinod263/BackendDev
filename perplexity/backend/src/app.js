6bc5decc39e86a0de701403a993d0ec89ddf2cf7
Vinod Gayari <gayarivinod263@gmail.com>
2026-04-06 14:58:16 +0530

    frontend integration

--- perplexity/backend/src/app.js
+++ perplexity/backend/src/app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev"))
 app.use(cors({
