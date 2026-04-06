52ebb7e0d1e2d17930af0d5e9d3757912b252936
Vinod Gayari <gayarivinod263@gmail.com>
2026-04-06 15:09:55 +0530

    api integration

--- perplexity/frontend/src/features/chat/service/chat.socket.js
+++ perplexity/frontend/src/features/chat/service/chat.socket.js
@@ -2,7 +2,7 @@ import { Rss } from "lucide-react";
 import { io } from "socket.io-client";
 
 export const initializeSocketConnection = () => {
-     const socket = io("http://localhost:3000", {
+     const socket = io("http://localhost:3000 ||https://backenddev-0f71.onrender.com/", {
         withCredentials: true,
     })
     socket.on("connect", ()=>{
